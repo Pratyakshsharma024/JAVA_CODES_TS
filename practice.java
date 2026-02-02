2c59409f7d2925df7419e936e09b7837d5257d9a
Pratyakshsharma024 <shourya699@gmail.com>
2026-02-02 14:41:14 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,21 @@
+public class practice{
+    public static void main(string[]args){
+        int[] a = {1,2,3,4,5};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+
+    }
+} 
+        int time = 0;
+        for (int i = 0; i < arr.length; i++) {
+           time += (max - arr[i]);
+            }
+System.out.println("time");
+        
+    }
+}
+
+    
+    
